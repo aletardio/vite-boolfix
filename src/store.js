54ff2fd91c4be2ff59e56fc8977f8c46c23a923c
aletardio <ale.tardio99@gmail.com>
2024-01-12 14:49:08 +0100

    created store.js and added endpoint

---
+++ src/store.js
@@ -0,0 +1,5 @@
+import { reactive } from 'vue';
+
+export const store = reactive ({
+    endpoint:'https://api.themoviedb.org/3/search/movie?api_key=5ddd19924641e4849b05e5860d73e272&language=it'
+})
