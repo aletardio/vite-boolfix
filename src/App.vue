0707b4332e22f408ff7a1d08e4cf665da59eb996
aletardio <ale.tardio99@gmail.com>
2024-01-12 14:56:51 +0100

    import AppHeader and store in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
+<script>
+
+import AppHeader from './components/AppHeader.vue';
+import { store } from './store.js'
+
+export default {
+  components: {
+    AppHeader
+  },
+  data() {
+    return {
+      store
+    }
+  },
+}
+</script>
 <template lang="">
   <div>
     
   </div>
 </template>
-<script>
-export default {
-  
-}
-</script>
-<style lang="">
-  
+<style lang="scss">
+  @use './styles/generals.scss' as *;
+  @use './styles/partials/variables' as *;
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+<style lang="scss" scoped>
+    
+</style>
