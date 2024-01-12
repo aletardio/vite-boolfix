e56d49968594ba099c88e3183b3aaa84aa4113fa
aletardio <ale.tardio99@gmail.com>
2024-01-12 15:34:53 +0100

    Created header with searchbar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,11 +7,14 @@ export default {
     <header>
         <div class="container-fluid">
             <div class="row">
-                <div class="col-6 d-flex align-items-center py-4">
+                <div class="col-9 d-flex align-items-center py-4">
                     <h1>Boolflix</h1>
                 </div>
-                <div class="col-6">
-                    
+                <div class="col-3 d-flex align-items-center justify-content-end">
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Titoli, persone, generi" aria-label="Titoli, persone, generi" aria-describedby="button-addon2">
+                        <button class="btn btn-danger" type="button" id="button-addon2">Cerca</button>
+                    </div>
                 </div>
             </div>
         </div>
@@ -22,8 +25,18 @@ export default {
 @use '../styles/partials/variables' as *;
 
     h1{
+        font-family: 'Bebas Neue', sans-serif;
         font-size: 50px;
         color: #E50815;
         padding: 5px 0;
     }
+
+    .input-group.mb-3 {
+    width: 75%;
+
+    button#button-addon2 {
+    background-color: #E50815;
+    }
+    }
+
 </style>
